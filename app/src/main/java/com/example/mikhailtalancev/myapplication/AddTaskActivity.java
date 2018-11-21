23682d553a08c3f3b1586da34d698d13cd63a81a
Mikhail Talancev <lighttenebris@gmail.com>
2018-11-21 12:13:55 +0200

    add_style_and_onClickListener

--- app/src/main/java/com/example/mikhailtalancev/myapplication/AddTaskActivity.java
+++ app/src/main/java/com/example/mikhailtalancev/myapplication/AddTaskActivity.java
@@ -5,14 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class AddTaskActivity extends AppCompatActivity {
+public class AddTaskActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+        btnAdd = (Button) findViewById(R.id.add_task);
+        btnAdd.setOnClickListener(this);
+
     }
+
 
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_without_add, menu);
@@ -40,4 +52,25 @@ public class AddTaskActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.add_task:
+                EditText name = (EditText) findViewById(R.id.add_task_name);
+                EditText description = (EditText) findViewById((R.id.task_description));
+                Spinner priority = (Spinner) findViewById(R.id.task_priority);
+                if (name.getText().toString().trim().length() == 0){
+                    Toast.makeText(this, "You did not enter a name", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (description.getText().toString().trim().length() == 0){
+                        Toast.makeText(this, "You did not enter a description", Toast.LENGTH_SHORT).show();
+                    } else {
+
+                    }
+                }
+
+                break;
+
+        }
+    }
 }
