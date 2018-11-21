4b08255ea5f95fd90c85f3cf82cb01b0e0f4a082
Mikhail Talancev <lighttenebris@gmail.com>
2018-11-21 14:17:16 +0200

    add_menu_to_new_activity

--- app/src/main/java/com/example/mikhailtalancev/myapplication/CyclicTaskActivity.java
+++ app/src/main/java/com/example/mikhailtalancev/myapplication/CyclicTaskActivity.java
@@ -1,7 +1,10 @@
 package com.example.mikhailtalancev.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class CyclicTaskActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class CyclicTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cyclic_task);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_without_add, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+
+
+            case R.id.profile:
+                Intent intent1 = new Intent(this, ProfileActivity.class);
+                startActivity(intent1);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
