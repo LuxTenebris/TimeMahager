be5e7934eb01c75bf8dbffab5ab5243b7f02ddfe
Mikhail Talancev <lighttenebris@gmail.com>
2018-11-21 14:31:30 +0200

    add_new_archive_activity

---
+++ app/src/main/java/com/example/mikhailtalancev/myapplication/ArchiveCyclicActivity.java
@@ -0,0 +1,41 @@
+package com.example.mikhailtalancev.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ArchiveCyclicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_archive_cyclic);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_without_add, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+
+
+            case R.id.profile:
+                Intent intent1 = new Intent(this, ProfileActivity.class);
+                startActivity(intent1);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
