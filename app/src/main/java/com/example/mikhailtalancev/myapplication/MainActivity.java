1e030e00fb417d4a8d87ef1c2ecaa97b44e5e6a3
Mikhail Talancev <lighttenebris@gmail.com>
2018-11-22 13:38:31 +0200

    optimise_import

--- app/src/main/java/com/example/mikhailtalancev/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mikhailtalancev/myapplication/MainActivity.java
@@ -2,19 +2,17 @@ package com.example.mikhailtalancev.myapplication;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import static com.example.mikhailtalancev.myapplication.R.*;
+import static com.example.mikhailtalancev.myapplication.R.id;
+import static com.example.mikhailtalancev.myapplication.R.layout;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
