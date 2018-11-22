8a62edd16204865e2ae6588c471a5229b314b31e
Mikhail Talancev <lighttenebris@gmail.com>
2018-11-22 13:53:14 +0200

    add_add_group

--- app/src/main/java/com/example/mikhailtalancev/myapplication/AddGroupActivity.java
+++ app/src/main/java/com/example/mikhailtalancev/myapplication/AddGroupActivity.java
@@ -1,19 +1,71 @@
 package com.example.mikhailtalancev.myapplication;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class AddGroupActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddGroupActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnAdd;
+    int DIALOG_DATE = 1;
+    int myYear = 2018;
+    int myMonth = 12;
+    int myDay = 14;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_group);
+        btnAdd = (Button) findViewById(R.id.add_group);
+        btnAdd.setOnClickListener(this);
+
     }
+
+
+    public void onclick(View view) {
+        showDialog(DIALOG_DATE);
+    }
+
+
+    protected Dialog onCreateDialog(int id) {
+        if (id == DIALOG_DATE) {
+            DatePickerDialog tpd = new DatePickerDialog(this, myCallBack, myYear, myMonth, myDay);
+            return tpd;
+        }
+        return super.onCreateDialog(id);
+    }
+
+    DatePickerDialog.OnDateSetListener myCallBack = new DatePickerDialog.OnDateSetListener() {
+
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            myYear = year;
+            myMonth = monthOfYear + 1;
+            myDay = dayOfMonth;
+        }
+    };
+
 
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_without_add, menu);
@@ -40,4 +92,46 @@ public class AddGroupActivity extends AppCompatActivity {
         }
     }
 
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    Map<String, Object> note = new HashMap<>();
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.add_group:
+                EditText name = (EditText) findViewById(R.id.add_group_name);
+                EditText description = (EditText) findViewById((R.id.group_description));
+                if (name.getText().toString().trim().length() == 0) {
+                    Toast.makeText(this, "You did not enter a name", Toast.LENGTH_SHORT).show();
+                } else {
+                    note.put("name", name.getText().toString());
+                    note.put("description", description.getText().toString());
+                    note.put("year", myYear);
+                    note.put("month", myMonth);
+                    note.put("day", myDay);
+
+                    db.collection("groups")
+                            .add(note)
+                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                                    Log.d("Tag", "DocumentSnapshot added with ID: " + documentReference.getId());
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.w("Tag", "Error adding document", e);
+                                }
+                            });
+                    Toast.makeText(this, "You add a group", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(this, MainActivity.class);
+                    startActivity(intent);
+                    break;
+
+                }
+        }
+    }
 }
