098ce3b5d572f99d906fbfd4c08e988396d1f275
Mikhail Talancev <lighttenebris@gmail.com>
2018-11-22 12:53:31 +0200

    add_new_add_activities

---
+++ app/src/main/java/com/example/mikhailtalancev/myapplication/AddGroupActivity.java
@@ -0,0 +1,43 @@
+package com.example.mikhailtalancev.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class AddGroupActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_group);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_without_add, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+
+
+            case R.id.profile:
+                Intent intent1 = new Intent(this, ProfileActivity.class);
+                startActivity(intent1);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
