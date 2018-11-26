d610e31be96601ce468bf1ef3a2a8a3deeee745d
Mikhail Talancev <lighttenebris@gmail.com>
2018-11-26 10:20:55 +0200

    add_list_view

--- app/src/main/java/com/example/mikhailtalancev/myapplication/DayTaskActivity.java
+++ app/src/main/java/com/example/mikhailtalancev/myapplication/DayTaskActivity.java
@@ -1,18 +1,16 @@
 package com.example.mikhailtalancev.myapplication;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup.LayoutParams;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,27 +18,14 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.google.protobuf.StringValue;
 
 import java.util.ArrayList;
-import java.util.concurrent.CountDownLatch;
-
-import static com.google.common.collect.ComparisonChain.start;
+import java.util.Collections;
 
 
 public class DayTaskActivity extends AppCompatActivity {
 
-
-    int[] colors = new int[3];
-
-
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-    ArrayList <String> name = new ArrayList<String>();
-    ArrayList <String> priority = new ArrayList<String>();
-    ArrayList <Long> dateYear = new ArrayList<Long>();
-    ArrayList <Long> dateMonth = new ArrayList<Long>();
-    ArrayList <Long> dateDay = new ArrayList<Long>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,58 +38,44 @@ public class DayTaskActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
+                            int i = 0;
+
+                            final String [] names = new String[task.getResult().getDocuments().size()];
 
                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                name.add(String.valueOf(document.get("name")));
-                                priority.add(String.valueOf(document.get("priority")));
-                                dateYear.add((Long) document.get("year"));
-                                dateMonth.add((Long) document.get("month"));
-                                dateDay.add((Long) document.get("day"));
 
                                 Log.d("TAG", document.getId() + " => " + document.get("name"));
+                                names[i] = (String) document.get("name");
+                                i++;
                             }
-                            colors[0] = Color.parseColor("#6773b7");
-                            colors[1] = Color.parseColor("#198f66");
-                            colors[2] = Color.parseColor("#89c3f1");
+                            ListView lvMain = (ListView) findViewById(R.id.lvDayTask);
 
-                            LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout);
+                            // создаем адаптер
+                            ArrayAdapter<String> adapter = new ArrayAdapter<String> (DayTaskActivity.this,
+                                    R.layout.day_task_item, names);
 
-                            LayoutInflater ltInflater = getLayoutInflater();
+                            // присваиваем адаптер списку
+                            lvMain.setAdapter(adapter);
 
-                            for (int i = 0; i < name.size(); i++) {
+                            lvMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                public void onItemClick(AdapterView<?> parent, View view,
+                                                        int position, long id) {
 
-                                View item = ltInflater.inflate(R.layout.item_solo, linLayout, false);
+                                    Intent intent = new Intent(DayTaskActivity.this, NoteActivity.class);
 
-                                TextView soloName = (TextView) item.findViewById(R.id.soloName);
-                                soloName.setText(name.get(i));
-                                TextView soloPriority = (TextView) item.findViewById(R.id.soloPriority);
-                                soloPriority.setText(priority.get(i));
-                                TextView soloDate = (TextView) item.findViewById(R.id.soloDate);
-                                soloDate.setText(String.valueOf(dateYear.get(i)) + "//" + String.valueOf(dateMonth.get(i)) + "//" + String.valueOf(dateDay.get(i)));
-                                item.getLayoutParams().width = LayoutParams.MATCH_PARENT;
+                                    Toast.makeText(DayTaskActivity.this,names[(int) id], Toast.LENGTH_SHORT).show();
 
-                                switch (priority.get(i)){
-                                    case "Middle":
-                                        item.setBackgroundColor(colors[0]);
-                                        break;
-                                    case "High" :
-                                        item.setBackgroundColor(colors[1]);
-                                        break;
-                                    case  "Low" :
-                                        item.setBackgroundColor(colors[2]);
-
+                                    startActivity(intent);
                                 }
-                                linLayout.addView(item);
-                            }
-
-
+                            });
                         } else {
                             Log.d("TAG", "Error getting documents: ", task.getException());
                         }
                     }
                 });
 
-            }
+        }
+
 
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_without_add, menu);
