48861b36787ee7b8f2d6f8a0f7e777ff4811bc43
Mikhail Talancev <lighttenebris@gmail.com>
2018-12-07 10:26:56 +0200

    add_style_to_add_cyclic_task

--- app/src/main/java/com/example/mikhailtalancev/myapplication/AddCyclicTaskActivity.java
+++ app/src/main/java/com/example/mikhailtalancev/myapplication/AddCyclicTaskActivity.java
@@ -12,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
@@ -23,21 +24,28 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.HashMap;
 import java.util.Map;
 
-public class AddCyclicTaskActivity extends AppCompatActivity implements View.OnClickListener {
+public class AddCyclicTaskActivity extends AppCompatActivity {
 
-    Button btnAdd;
+    TextView StartV;
+    TextView EndV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_cyclic_task);
 
-        btnAdd = (Button) findViewById(R.id.add_cyclic_task);
-        btnAdd.setOnClickListener(this);
+        StartV = findViewById(R.id.setTimeCyclicStart);
+        EndV = findViewById(R.id.setTimeCyclicEnd);
     }
 
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    Map<String, Object> note = new HashMap<>();
+
+
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_without_add, menu);
+        getMenuInflater().inflate(R.menu.menu_add, menu);
         return true;
     }
 
@@ -45,6 +53,40 @@ public class AddCyclicTaskActivity extends AppCompatActivity implements View.OnC
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle item selection
         switch (item.getItemId()) {
+            case R.id.addNote:
+
+                EditText name = (EditText) findViewById(R.id.add_cyclic_task_name);
+                EditText description = (EditText) findViewById((R.id.cyclicTask_description));
+                if (name.getText().toString().trim().length() == 0) {
+                    Toast.makeText(this, "You did not enter a name", Toast.LENGTH_SHORT).show();
+                } else {
+                    if(StartV.getText().toString().equals("Start time of your task") || EndV.getText().toString().equals("End time of your task")){
+                        Toast.makeText(this,"You did not choose a time", Toast.LENGTH_SHORT).show();
+                    } else {
+                        note.put("name", name.getText().toString());
+                        note.put("description", description.getText().toString());
+                        db.collection("cyclicTasks")
+                                .add(note)
+                                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                    @Override
+                                    public void onSuccess(DocumentReference documentReference) {
+                                        Log.d("Tag", "DocumentSnapshot added with ID: " + documentReference.getId());
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.w("Tag", "Error adding document", e);
+                                    }
+                                });
+                        Toast.makeText(this, "You add a cyclicTask", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+                return true;
+
             case R.id.settings:
                 Intent intent = new Intent(this, SettingsActivity.class);
                 startActivity(intent);
@@ -64,23 +106,30 @@ public class AddCyclicTaskActivity extends AppCompatActivity implements View.OnC
     int DIALOG_TIME = 1;
     int myHour = 14;
     int myMinute = 0;
+    int myHourE = 14;
+    int myMinuteE = 0;
+
 
     public void onclick(View view) {
         showDialog(DIALOG_TIME);
-        note.put("HourStart", String.valueOf(myHour));
-        note.put("MinuteStart", String.valueOf(myMinute));
+
     }
 
     public void onclick1(View view){
-        showDialog(DIALOG_TIME);
-        note.put("HourEnd", String.valueOf(myHour));
-        note.put("MinuteEnd", String.valueOf(myMinute));
+        showDialog(0);
+
     }
     protected Dialog onCreateDialog(int id) {
         if (id == DIALOG_TIME) {
             TimePickerDialog tpd = new TimePickerDialog(this, myCallBack, myHour, myMinute, true);
             return tpd;
         }
+
+        if (id == 0) {
+            TimePickerDialog tpd = new TimePickerDialog(this, myCallBack1, myHourE, myMinuteE, true);
+            return tpd;
+        }
+
         return super.onCreateDialog(id);
     }
 
@@ -88,51 +137,43 @@ public class AddCyclicTaskActivity extends AppCompatActivity implements View.OnC
         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
             myHour = hourOfDay;
             myMinute = minute;
+            note.put("HourStart", String.valueOf(myHour));
+            note.put("MinuteStart", String.valueOf(myMinute));
+
+            String hourStart = String.valueOf(myHour);
+            if(hourStart.length() == 1) {
+                hourStart = "0" + hourStart;
+            }
+
+            String MinuteStart = String.valueOf(myMinute);
+            if(MinuteStart.length() == 1) {
+                MinuteStart = "0" + MinuteStart;
+            }
+
+            StartV.setText(hourStart + ":" + MinuteStart);
         }
     };
 
+    TimePickerDialog.OnTimeSetListener myCallBack1 = new TimePickerDialog.OnTimeSetListener() {
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            myHourE= hourOfDay;
+            myMinuteE = minute;
+            note.put("HourEnd", String.valueOf(myHourE));
+            note.put("MinuteEnd", String.valueOf(myMinuteE));
 
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
+            String hourStart = String.valueOf(myHourE);
+            if(hourStart.length() == 1) {
+                hourStart = "0" + hourStart;
+            }
 
-    Map<String, Object> note = new HashMap<>();
+            String MinuteStart = String.valueOf(myMinuteE);
+            if(MinuteStart.length() == 1) {
+                MinuteStart = "0" + MinuteStart;
+            }
 
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.add_cyclic_task:
-                EditText name = (EditText) findViewById(R.id.add_cyclic_task_name);
-                EditText description = (EditText) findViewById((R.id.cyclicTask_description));
-                if (name.getText().toString().trim().length() == 0) {
-                    Toast.makeText(this, "You did not enter a name", Toast.LENGTH_SHORT).show();
-                } else {
-                    note.put("name", name.getText().toString());
-                    note.put("description", description.getText().toString());
-                    db.collection("cyclicTasks")
-                            .add(note)
-                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                                @Override
-                                public void onSuccess(DocumentReference documentReference) {
-                                    Log.d("Tag", "DocumentSnapshot added with ID: " + documentReference.getId());
-                                }
-                            })
-                            .addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Log.w("Tag", "Error adding document", e);
-                                }
-                            });
-                    Toast.makeText(this, "You add a cyclicTask", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                    break;
-
-                }
+            EndV.setText(hourStart + ":" + MinuteStart);
         }
-
-    }
-
+    };
 
     @Override
     public void onBackPressed() {
